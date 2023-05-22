5c22e8cd7aa5222a5e2642142039ccd1e432e8ae
Ryomm <matsusaka.ryoko@nifty.co.jp>
2023-05-22 16:49:09 +0900

    transform multi module

---
+++ multimodule/SandboxMultiModule/Feature/Sources/Models/ComplexNum.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by SCI02122 on 2023/05/22.
+//
+
+import Foundation
--- multimodule/SandboxMultiModule/Root/Sources/Root/Root.swift
+++
@@ -1,6 +0,0 @@
-public struct Root {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
